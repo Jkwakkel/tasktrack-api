077250a656088f59d4478ae081407ccf541d93fa
jkwakkel <jkwakkel@xs4all.nl>
2025-07-12 17:08:17 +0200

    feat: add model, migration and factory for tasks
    - User: Add relationship for Task

---
+++ app/Models/Task.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Task extends Model
+{
+    /** @use HasFactory<\Database\Factories\TaskFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+
+    public function users(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
